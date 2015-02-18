81a35fb505442cdfcb33b40f6a17fed500cd4b63
Nebojsa Petrovic <nebspetrovic@gmail.com>
2015-02-17 19:43:09 -0800

    Remove unnecessary comments and template methods

--- Kard/AppDelegate.h
+++ Kard/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Kard
-//
-//  Created by Nebojsa Petrovic on 2/17/15.
-//  Copyright (c) 2015 Nebojsa Petrovic. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
